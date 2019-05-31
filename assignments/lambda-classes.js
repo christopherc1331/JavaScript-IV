b1a66cf35a221b99675713b57de60758dd29306a
Chris Cruz <christopherc1331@gmail.com>
2019-05-30 20:00:13 -0700

    Finished stretch

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,5 @@
 // CODE here for your Lambda Classes
-
+//"Master" Person class
 class Person {
     constructor(attr) {
         this.name = attr.name;
@@ -10,7 +10,7 @@ class Person {
         console.log(`Hello, my name is ${this.name}, I am from ${this.location}.`)
     }
 }
-
+//Instructor class inherits Person
 class Instructor extends Person {
     constructor(attr) {
         super(attr);
@@ -21,17 +21,35 @@ class Instructor extends Person {
     demo(subject) {
         console.log(`Today we are learning about ${this.subject}.`)
     }
-    grade(student, subject) {
+    gradeAssignment(student, subject) {
         console.log(`${student.name} receives a perfect score on ${subject}.`)
     }
+    adjustGrade(student) {
+        let num = Math.floor(Math.random() * 99) + 1;
+        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        if (this.num > 0) {
+            console.log(`${this.name} adds ${num} points.`)
+        } else {
+            console.log(`${this.name} deducts ${num} points`)
+        }
+        let newGrade = student.grade;
+        newGrade += num;
+        console.log(`${student.name}'s new grade: ${student.newGrade}. `)
+        if (newGrade > 70) {
+            console.log(`Congratulations, ${student.name}! You've graduated from lambda! You're the bee's knees homie!`);
+        } else {
+            adjustGrade(student);
+        }
+    }
 }
-
+//Student class inherits Person
 class Student extends Person {
     constructor(attr) {
         super(attr);
         this.previousBackground = attr.previousBackground;
         this.className = attr.className;
         this.favSubjects = attr.favSubjects;
+        this.grade = 50;
     }
     listsSubjects() {
         this.favSubjects.forEach(function (item) {
@@ -45,7 +63,7 @@ class Student extends Person {
         console.log(`${this.name} has begun sprint challenge on ${subject}.`)
     }
 }
-
+//ProjectManager class inherits Instructor
 class ProjectManager extends Instructor {
     constructor(attr) {
         super(attr);
@@ -61,7 +79,7 @@ class ProjectManager extends Instructor {
 }
 
 
-// const test = new Student({ name: "Geff", favSubjects: ["JFV", "Nev"] });
+const test = new Student({ name: "Geff", favSubjects: ["JFV", "Nev"] });
 
-// test.PRAssignment("Django");
-// test.sprintChallenge("Django");
+test.PRAssignment("Django");
+test.sprintChallenge("Django");
